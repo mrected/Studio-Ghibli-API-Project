5125a54c32ce457379672e82741cfca73f992fc5
mrected <chris.terrazas@gmail.com>
2022-08-21 20:49:12 -0400

    fetch works / titles display

--- studio ghibli API/src/server.js
+++ studio ghibli API/src/server.js
@@ -1,13 +1,4 @@
-import fetch from 'node-fetch';
-fetch("https://ghibliapi.herokuapp.com/films")
-    .then(response => response.json())
-    .then(films => {
-        films.filter(titles =>  {
-            let movieTitles = titles.title;
-            console.log(movieTitles)
-           
-        }) 
-    })
+// import fetch from 'node-fetch';
 
 
 
